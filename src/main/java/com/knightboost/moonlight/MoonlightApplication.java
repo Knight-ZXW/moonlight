385e8335d84fb8671825dd75b65a4411dec341aa
Knight-ZXW <nimdanoob@163.com>
2021-10-27 23:41:35 +0800

    init project

---
+++ src/main/java/com/knightboost/moonlight/MoonlightApplication.java
@@ -0,0 +1,13 @@
+package com.knightboost.moonlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonlightApplication.class, args);
+	}
+
+}
